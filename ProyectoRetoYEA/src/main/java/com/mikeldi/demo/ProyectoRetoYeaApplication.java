519f8d6ba173f76c8c36e87093d41256152b6759
ebanor <ebanor@a.mikeldi.com>
2025-10-24 12:34:18 +0200

    comiteando bby

---
+++ ProyectoRetoYEA/src/main/java/com/mikeldi/demo/ProyectoRetoYeaApplication.java
@@ -0,0 +1,13 @@
+package com.mikeldi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRetoYeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRetoYeaApplication.class, args);
+	}
+
+}
---
+++ ProyectoRetoYEA/src/test/java/com/mikeldi/demo/ProyectoRetoYeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikeldi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoRetoYeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
